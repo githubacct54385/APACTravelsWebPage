6d396ae123b7a9982f444185d5ad2be99b793aec
Alex <alexbarke002@gmail.com>
2017-12-06 12:21:12 +0530

    First Commit

---
+++ myapp.rb
@@ -0,0 +1,56 @@
+require 'sinatra'
+require 'stripe'
+
+set :publishable_key, ENV['PUBLISHABLE_KEY']
+set :secret_key, ENV['SECRET_KEY']
+
+Stripe.api_key = settings.secret_key
+
+get '/' do
+  erb :index
+end
+
+post '/chargeSingapore' do
+  # Amount in cents
+  @amount = 80000
+
+  customer = Stripe::Customer.create(
+    :email => 'customer@example.com',
+    :source  => params[:stripeToken]
+  )
+
+  charge = Stripe::Charge.create(
+    :amount      => @amount,
+    :description => 'Sinatra Charge - Singapore Flight + Hotel',
+    :currency    => 'usd',
+    :customer    => customer.id
+  )
+
+  @dest = "Singapore"
+  erb :charge
+end
+
+post '/chargeThailand' do
+	@amount = 80000
+
+	customer = Stripe::Customer.create(
+    :email => 'customer@example.com',
+    :source  => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+    :amount      => @amount,
+    :description => 'Sinatra Charge - Thailand Flight + Hotel',
+    :currency    => 'usd',
+    :customer    => customer.id
+    )
+
+	@dest = "Thailand"
+  	erb :charge
+    #erb :charge, :locals => @amount
+end
+
+
+error Stripe::CardError do
+  env['sinatra.error'].message
+end
