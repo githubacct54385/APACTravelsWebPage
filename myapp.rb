bdfd8ed553f5c8b07d3bba3ae97a771d30e3e31b
Alex <alexbarke002@gmail.com>
2017-12-31 15:21:01 +0530

    Heroku push

--- myapp.rb
+++ myapp.rb
@@ -28,16 +28,19 @@ post '/webhook' do
   # Retrieve the event from Stripe
   @event = Stripe::Event.retrieve(@event_json['id'])
 
+
+  SendEmailUsingTemplateJson('alexbarke002@gmail.com', 'Singapore')
+  status 200
   # Only respond to `invoice.payment_succeeded` events
-  if @event.type.eql?('charge.succeeded')
+  #if @event.type.eql?('charge.succeeded')
     # Send a receipt for the invoice 
-    unless @event.data.object.charge.nil?
-      SendEmailUsingTemplateJson('alexbarke002@gmail.com', 'Singapore')
-    end
-  else 
+   # unless @event.data.object.charge.nil?
+   #   SendEmailUsingTemplateJson('alexbarke002@gmail.com', 'Singapore')
+    #end
+  #else 
     # Nothing to see here, return a 200
-    status 200
-  end
+   # status 200
+  #end
 end
 
 
