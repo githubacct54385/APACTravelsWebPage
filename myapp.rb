1259005b0891edf2597e9087f2c6f3f3f1a6f033
Alex <alexbarke002@gmail.com>
2017-12-31 16:27:22 +0530

    Heroku push

--- myapp.rb
+++ myapp.rb
@@ -14,6 +14,22 @@ get '/' do
   erb :index
 end
 
+post '/WebhookPlainEmail' do
+  from = Email.new(email: ENV['SUPPORT_EMAIL'])
+  to = Email.new(email: 'alexbarke002@gmail.com')
+  subject = 'Sending with SendGrid is Fun'
+  content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+  mail = Mail.new(from, subject, to, content)
+
+  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+  response = sg.client.mail._('send').post(request_body: mail.to_json)
+  puts response.status_code
+  puts response.body
+  puts response.headers
+
+  status 200
+end
+
 post '/webhook' do
 
   puts "Enter webhook code"
