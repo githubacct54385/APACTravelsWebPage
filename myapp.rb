62516455ba648c647346e5658566697da28c4005
Alex <alexbarke002@gmail.com>
2017-12-31 14:21:42 +0530

    Heroku push

--- myapp.rb
+++ myapp.rb
@@ -16,9 +16,9 @@ end
 
 post '/webhook' do
   
-  @charge = Stripe::Charge.retrieve("ch_1Bf1QLFb0ie2UiOWOCLtwkOx")
-  @customerEmail = charge.customer.email
-  SendChargeSucceedEmail(@customerEmail)
+  #@charge = Stripe::Charge.retrieve("ch_1Bf1QLFb0ie2UiOWOCLtwkOx")
+  #@customerEmail = charge.customer.email
+  #SendChargeSucceedEmail(@customerEmail)
 
   # Retrieve the request's body and parse it as JSON
   #event_json = JSON.parse(request.body.read)
@@ -27,17 +27,17 @@ post '/webhook' do
   #@event = Stripe::Event.retrieve(event_json['id'])
 
   # Retrieve the request's body and parse it as JSON
-  #event_json = JSON.parse(request.body.read)
+  event_json = JSON.parse(request.body.read)
 
   # Retrieve the event from Stripe
-  #@event = Stripe::Event.retrieve(event_json['id'])
+  @event = Stripe::Event.retrieve(event_json['id'])
 
   # Only respond to `invoice.payment_succeeded` events
-  #if @event.type.eql?('charge.succeeded')
-  #  @charge = Stripe::Charge.retrieve("ch_1Bf1QLFb0ie2UiOWOCLtwkOx")
-  #  @customerEmail = charge.customer.email
-  #  SendChargeSucceedEmail(@customerEmail)
-  #end
+  if @event.type.eql?('charge.succeeded')
+    #@charge = Stripe::Charge.retrieve("ch_1Bf1QLFb0ie2UiOWOCLtwkOx")
+    #@customerEmail = charge.customer.email
+    SendChargeSucceedEmail('alexbarke002@gmail.com')
+  end
 
   #SendEmailUsingTemplateJson('alexbarke002@gmail.com', 'Singapore')
 
