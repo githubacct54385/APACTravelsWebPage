f1d283a0dfd780c7f873bf60f9b0303c85b2f2e9
Alex <alexbarke002@gmail.com>
2018-01-01 12:46:09 +0530

    Heroku push

--- myapp.rb
+++ myapp.rb
@@ -16,22 +16,22 @@ end
 
 post '/webhook2' do
 
-  @sendEmail = 'alexbarke002@gmail.com'
-  @dest = 'Singapore'
-  SendEmailUsingTemplateJson(@sendEmail, @dest)
+  #@sendEmail = 'alexbarke002@gmail.com'
+  #@dest = 'Singapore'
+  #SendEmailUsingTemplateJson(@sendEmail, @dest)
 
 
-  #from = Email.new(email: 'test@example.com')
-  #to = Email.new(email: 'alexbarke002@gmail.com')
-  #subject = 'Sending with SendGrid is Fun'
-  #content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
-  #mail = Mail.new(from, subject, to, content)
+  from = Email.new(email: 'test@example.com')
+  to = Email.new(email: 'alexbarke002@gmail.com')
+  subject = 'Sending with SendGrid is Fun'
+  content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+  mail = Mail.new(from, subject, to, content)
 
-  #sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-  #response = sg.client.mail._('send').post(request_body: mail.to_json)
-  #puts response.status_code
-  #puts response.body
-  #puts response.headers
+  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+  response = sg.client.mail._('send').post(request_body: mail.to_json)
+  puts response.status_code
+  puts response.body
+  puts response.headers
 
   status 200
 end
